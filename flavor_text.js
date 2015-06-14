e02dfbcd96909b222662df42cbc05e1f09cdd11f
Andrew <Hobodoken@users.noreply.github.com>
2015-06-13 22:54:03 -0400

    Update flavor_text.js

--- flavor_text.js
+++ flavor_text.js
@@ -11,19 +11,19 @@ flavor_text = {
             'buried alive'
             'thrown off of a high place'
             'buried neck deep and smothered with honey'
-            ' stabbed, then kicked, then repeatedly beaten with sticks in the groin'
-            ' locked in a room with a swarm of pissed off bees'
-            ' stoned by the children of the village'
-            ' forcefed hot coals'
-            ' by snoo snoo'
-            ' impaled professionally'
-            ' crushed by several large bolders'
-            ' drawn and quartered'
-            ' tar and feathered'
-            ' stripped naked, slathered in cows blood, then given a 10 second head start before the hounds were released'
-            ' thrown into a pit of poisonous snakes'
-            ' thrown into a bear pit'
-            ' '
+            'stabbed, then kicked, then repeatedly beaten with sticks in the groin'
+            'locked in a room with a swarm of pissed off bees'
+            'stoned by the children of the village'
+            'forcefed hot coals'
+            'by snoo snoo'
+            'impaled professionally'
+            'crushed by several large bolders'
+            'drawn and quartered'
+            'tar and feathered'
+            'stripped naked, slathered in cows blood, then given a 10 second head start before the hounds were released'
+            'thrown into a pit of poisonous snakes'
+            'thrown into a bear pit'
+            'crucified'
             
             
             
@@ -39,27 +39,30 @@ flavor_text = {
             'eaten'
             'eviscerated'
             'lethally stubbed toe'
-            ' head crushed in'
+            'head crushed in'
             'missing your botton half, dragging yourself towards help'
+            'missing your eyes'
+            'without a head'
             
         ],
     "Location" :
         [
-            ' the Church House',
+            'the Church House',
             'the Town Square',
             'the Library'
             'the Well'
-            'the Old Jenkins Place
-            'the Magistrate's office'
-            ' the edge of town'
-            ' the barn where you lost your virginity'
-            ' the place where you felt most safe in the world'
+            'the ol\' Jenkins Place
+            'the Magistrate\'s office'
+            'the edge of town'
+            'Tommy\' pub'
+            'the barn where you lost your virginity'
+            'the place where you felt most safe in the world'
             'that place mom said to stay away from'
-            ' a grassy knoll'
-            ' a hill down by the river'
-            ' the dog kennel'
+            'a grassy knoll'
+            'a hill down by the river'
+            'the dog kennel'
             'the abandoned mill'
-            ' the spookiest place you can think of'
+            'the spookiest place you can think of'
             
         ]
 }
