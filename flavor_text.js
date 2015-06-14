4b17cee604668dc5bf86a5db1552cfd4dd6b5ef7
Brian Stascavage <brian@stascavage.com>
2015-06-13 22:06:53 -0400

    Forgot flavor_text.js

---
+++ flavor_text.js
@@ -0,0 +1,20 @@
+flavor_text = {
+    "Village" :
+        [
+            'Burned at the stake',
+            'Hung',
+            'Killed in a bar fight'
+        ],
+    "Werewolf" :
+        [
+            'Mauled at midnight',
+            'Throat torn out',
+            'ACL torn'
+        ],
+    "Location" :
+        [
+            'Church House',
+            'Town Square',
+            'Library'
+        ]
+}
