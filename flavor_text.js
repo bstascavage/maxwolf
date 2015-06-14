cc713953d1ec96fe7a9d344aef3a9bf6c8876bf8
Kirbysabamf <rluckay@gmail.com>
2015-06-13 22:50:56 -0400

    Update flavor_text.js

--- flavor_text.js
+++ flavor_text.js
@@ -1,28 +1,65 @@
+// death variable at location variable
+
 flavor_text = {
     "Village" :
         [
             'Burned at the stake',
             'Hung',
             'Killed in a bar fight'
-            'Crucified'
+            'drowned'
+            'kicked repeatedly by schoolchildren'
+            'buried alive'
+            'thrown off of a high place'
+            'buried neck deep and smothered with honey'
+            ' stabbed, then kicked, then repeatedly beaten with sticks in the groin'
+            ' locked in a room with a swarm of pissed off bees'
+            ' stoned by the children of the village'
+            ' forcefed hot coals'
+            ' by snoo snoo'
+            ' impaled professionally'
+            ' crushed by several large bolders'
+            ' drawn and quartered'
+            ' tar and feathered'
+            ' stripped naked, slathered in cows blood, then given a 10 second head start before the hounds were released'
+            ' thrown into a pit of poisonous snakes'
+            ' thrown into a bear pit'
+            ' '
+            
+            
+            
+            
         ],
     "Werewolf" :
         [
             'Mauled at midnight',
             'Throat torn out',
             'ACL torn'
-            'Eviscerated'
-            'Missing their eyes'
-            'A bloody mess'
+            'crushed'
+            'torn into smithereen'
+            'eaten'
+            'eviscerated'
+            'lethally stubbed toe'
+            ' head crushed in'
+            'missing your botton half, dragging yourself towards help'
+            
         ],
     "Location" :
         [
-            'Church House',
-            'Town Square',
-            'Library'
-            'The old fishin hole'
-            'The tavern'
-            'The brothel'
-            'Town limits'
+            ' the Church House',
+            'the Town Square',
+            'the Library'
+            'the Well'
+            'the Old Jenkins Place
+            'the Magistrate's office'
+            ' the edge of town'
+            ' the barn where you lost your virginity'
+            ' the place where you felt most safe in the world'
+            'that place mom said to stay away from'
+            ' a grassy knoll'
+            ' a hill down by the river'
+            ' the dog kennel'
+            'the abandoned mill'
+            ' the spookiest place you can think of'
+            
         ]
 }
