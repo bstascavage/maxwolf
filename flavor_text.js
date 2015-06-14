d79a989305ac52ccfe6d276f3281f9f805e2027a
wdonnell <william.c.donnell@gmail.com>
2015-06-13 23:09:58 -0400

    fixing json

--- flavor_text.js
+++ flavor_text.js
@@ -5,61 +5,61 @@ flavor_text = {
         [
             'Burned at the stake',
             'Hung',
-            'Killed in a bar fight'
-            'drowned'
-            'kicked repeatedly by school children'
-            'buried alive'
-            'thrown off of a high place'
-            'buried neck deep and smothered with honey'
-            'stabbed, then kicked, then repeatedly beaten with sticks in the groin'
-            'locked in a room with a swarm of pissed off bees'
-            'stoned by the children of the village'
-            'forcefed hot coals'
-            'by snoo snoo'
-            'impaled professionally'
-            'crushed by several large bolders'
-            'drawn and quartered'
-            'tar and feathered'
-            'stripped naked, slathered in cows blood, then given a 10 second head start before the hounds were released'
-            'thrown into a pit of poisonous snakes'
-            'thrown into a bear pit'
-            'hung upsidedown  '
-            'by village bukkake'
-            'by beheading'
-            'by poisoning'
-            'by fored blood transfusion'
-            'crucified'
+            'Killed in a bar fight',
+            'drowned',
+            'kicked repeatedly by school children',
+            'buried alive',
+            'thrown off of a high place',
+            'buried neck deep and smothered with honey',
+            'stabbed, then kicked, then repeatedly beaten with sticks in the groin',
+            'locked in a room with a swarm of pissed off bees',
+            'stoned by the children of the village',
+            'forcefed hot coals',
+            'by snoo snoo',
+            'impaled professionally',
+            'crushed by several large bolders',
+            'drawn and quartered',
+            'tar and feathered',
+            'stripped naked, slathered in cows blood, then given a 10 second head start before the hounds were released',
+            'thrown into a pit of poisonous snakes',
+            'thrown into a bear pit',
+            'hung upsidedown  ',
+            'by village bukkake',
+            'by beheading',
+            'by poisoning',
+            'by fored blood transfusion',
+            'crucified',
             'given a lethal lapjob'
         ],
     "Werewolf" :
         [
             'Mauled at midnight',
             'Throat torn out',
-            'ACL torn'
-            'crushed'
-            'torn into several gnawed pieces'
-            'eaten'
-            'eviscerated'
-            'lethally stubbed toe'
-            'head crushed in'
+            'ACL torn',
+            'crushed',
+            'torn into several gnawed pieces',
+            'eaten',
+            'eviscerated',
+            'lethally stubbed toe',
+            'head crushed in',
             'missing your botton half, dragging yourself towards help'
         ],
     "Location" :
         [
             'the Church House',
             'the Town Square',
-            'the Library'
-            'the Well'
-            'the Old Jenkins Place'
-            'the Magistrate\'s office'
-            'the edge of town'
-            'the barn where you lost your virginity'
-            'the place where you felt most safe in the world'
-            'that place mom said to stay away from'
-            'a grassy knoll'
-            'a hill down by the river'
-            'the dog kennel'
-            'the abandoned mill'
+            'the Library',
+            'the Well',
+            'the Old Jenkins Place',
+            'the Magistrate\'s office',
+            'the edge of town',
+            'the barn where you lost your virginity',
+            'the place where you felt most safe in the world',
+            'that place mom said to stay away from',
+            'a grassy knoll',
+            'a hill down by the river',
+            'the dog kennel',
+            'the abandoned mill',
             'the spookiest place you can think of'
         ]
 }
